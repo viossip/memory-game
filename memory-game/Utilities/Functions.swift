76615941fe7a0b9b2c34571b6abb3afc41bd0f9d
Vitaly <vitaly@Orens-MacBook-Air.local>
2018-04-09 21:48:15 +0300

    new viewControllers

--- memory-game/Utilities/Functions.swift
+++ memory-game/Utilities/Functions.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+public func className(_ aClass: AnyClass) -> String {
+    let className = NSStringFromClass(aClass)
+    let components = className.components(separatedBy: ".")
+    
+    if components.count > 0 {
+        return components.last!
+    } else {
+        return className
+    }
+}
+
+//func stretchToSuperViewEdges(_ insets: UIEdgeInsets = UIEdgeInsets.zero) {
+//    // Validate
+//    guard let superview = superview else { fatalError("superview not set") }
+//    
+//    let leftConstraint = constraintWithItem(superview, attribute: .left, multiplier: 1, constant: insets.left)
+//    let topConstraint = constraintWithItem(superview, attribute: .top, multiplier: 1, constant: insets.top)
+//    let rightConstraint = constraintWithItem(superview, attribute: .right, multiplier: 1, constant: insets.right)
+//    let bottomConstraint = constraintWithItem(superview, attribute: .bottom, multiplier: 1, constant: insets.bottom)
+//    
+//    let edgeConstraints = [leftConstraint, rightConstraint, topConstraint, bottomConstraint]
+//    
+//    translatesAutoresizingMaskIntoConstraints = false
+//    
+//    superview.addConstraints(edgeConstraints)
+//}
+//
+//func constraintWithItem(_ view: UIView, attribute: NSLayoutAttribute, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint {
+//    return NSLayoutConstraint(item: self, attribute: attribute, relatedBy: .equal, toItem: view, attribute: attribute, multiplier: multiplier, constant: constant)
+//}
+
--- memory-game/ViewControllers/GameViewController.swift
+++ memory-game/ViewControllers/GameViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
-    
-    enum ButtonID: String {
-        case ButtonEnd = "endGame"
-        case ButtonNew = "newGame"
-        case ButtonPause = "pauseGame"
-    }
+enum Level: Int {
+    case Easy = 3 , Medium = 4, Hard = 5
+}
 
+enum ButtonID: String {
+    case ButtonEnd = "endGame"
+    case ButtonNew = "newGame"
+    case ButtonPause = "pauseGame"
+}
+
+class GameViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var level = Level.Easy
+    var game = GameLogic()
+    var timer:Timer?
+    let TileMargin = CGFloat(5.0)
+    
+    var NumberOfRows = 3
+    var NumberOfColumns = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,14 +38,42 @@ class GameViewController: UIViewController {
                 btn.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
             }
         }
+        
+        initGame()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func initGame() {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UICollectionViewCell
+        
+        return cell;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        collectionView.backgroundColor = UIColor.clear
+        return NumberOfColumns
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+        let rowsCount = CGFloat(NumberOfColumns)
+        let dimentions = collectionView.frame.height / rowsCount - (rowsCount * TileMargin * 0.8)
+        
+        return CGSize(width: dimentions, height: dimentions) // collectionView.frame.height * 0.9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(TileMargin, TileMargin, TileMargin, TileMargin)
+    }
+    
     @objc func buttonPressed(_ button: UIButton) {
-        print("+++++++++ " + button.restorationIdentifier!)
+        //print("+++++++++ " + button.restorationIdentifier!)
         if let btnId = ButtonID(rawValue: button.restorationIdentifier!) {
             
             switch btnId {
@@ -42,9 +84,24 @@ class GameViewController: UIViewController {
             case .ButtonEnd:
                 dismiss(animated: true, completion: nil)
             }
-            
         }
     }
+}
 
-
+class GameCell: UICollectionViewCell {
+    
+    var playerMarkLabel: UILabel!
+    
+    override func awakeFromNib() {
+        //📘("Created a \(className(self.classForCoder)) object")
+    }
+    
+    func configCell() {
+//        self.backgroundColor = UIColor.red
+//        self.playerMarkLabel.text = ""
+    }
+    
+    func openCell(_ cellID: String) {
+        
+    }
 }
