0c5f9e2eaa576e3fe4de50089dc4b9b8d91435cf
ilya <ilya@ilyas-Mac.local>
2018-06-07 13:27:35 +0300

    High Scores Update

--- memory-game/ViewControllers/HighscorsViewController.swift
+++ memory-game/ViewControllers/HighscorsViewController.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class HighscorsViewController: UIViewController {
 //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
+//
 //    }
 //
 //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
+//
 //    }
-    
+//    
     
     
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        	
     }
 
     override func didReceiveMemoryWarning() {
